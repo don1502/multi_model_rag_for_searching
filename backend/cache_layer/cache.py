fb04fb1bdf69971f2562d89284b3586f1a2747ac
Ajaysvasan <ajay192006@gmail.com>
2026-02-03 12:42:59 +0100

    init: cache layer'

---
+++ backend/cache_layer/cache.py
@@ -0,0 +1,4 @@
+# This is the cache layer pipeline
+# Need to implement L1 cache , L2 cache , L3 cache
+# Logic to deside which topic reside on which layer and which topic to forget
+# and also a way to store the cache in way that the exsisting cache gets loaded automatically when the application is started
